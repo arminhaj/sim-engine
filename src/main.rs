d40391f2498907d46f682d0cf6656216886e2794
arminhaj <48605166+arminhaj@users.noreply.github.com>
2026-09-08 10:13:36 -0400

    refactored worker thread as a tokio task

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,11 @@ use app::{App, UserEvent};
 use log::debug;
 use std::sync::Arc;
 use tokio::sync::oneshot;
-use winit::{
-    event_loop::EventLoop,
-    window::Window,
-};
+use winit::error::EventLoopError;
+use winit::{event_loop::EventLoop, window::Window};
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() -> anyhow::Result<(), EventLoopError> {
     // initialize logger
     env_logger::init();
 
@@ -23,36 +21,27 @@ async fn main() -> anyhow::Result<()> {
     let event_loop = EventLoop::<UserEvent>::with_user_event().build()?;
     let proxy = event_loop.create_proxy();
 
-    debug!("Spawning thread");
-    let handle = std::thread::spawn(async move || -> anyhow::Result<()> {
+    debug!("Spawning task");
+    tokio::spawn(async move {
         debug!("Emitting UserEvent");
 
         // wait to receive the Window from App::resumed()
-        let window = rx.blocking_recv()?;
+        let window = rx.blocking_recv().unwrap();
 
         // now we can safely use the proxy.
         debug!("Obtained window {window:?}");
         let _ = proxy.send_event(UserEvent::WakeUp);
 
-        worker_main(window).await;
-        Ok(())
+        worker_main(window);
     });
 
-
-    let _ = event_loop.run_app(&mut app);
-
-    let join_result = handle.join();
-    if let Err(err) = join_result {
-        std::panic::resume_unwind(err);
-    }
-
-    let worker_result = join_result.unwrap().await;
-    worker_result
+    event_loop.run_app(&mut app)
 }
 
 /// Serve as an entry point for the worker thread
-async fn worker_main(window: Arc<Window>) {
+fn worker_main(window: Arc<Window>) {
     debug!("This is the worker thread's entry point to the program.");
+    unimplemented!();
+
     let canvas = draw::Canvas::new(window).await;
-    
 }
