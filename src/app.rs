d7eee35c797f31bd95bb1981484c3835760854d7
arminhaj <48605166+arminhaj@users.noreply.github.com>
2026-09-10 15:05:32 -0400

    Implemented instantiation of wgpu state on main thread through UserEvent

--- src/app.rs
+++ src/app.rs
@@ -1,9 +1,21 @@
-use log::{error, info};
+use anyhow::{Context, anyhow};
+use pollster::FutureExt;
 use std::error::Error;
 use std::sync::Arc;
+use tokio::runtime::Handle;
 use tokio::sync::oneshot::Sender;
-use wgpu::rwh::{HasDisplayHandle, HasWindowHandle, WindowHandle, RawWindowHandle, HandleError, AppKitWindowHandle};
+use tokio::task;
+use tracing::{Level, debug, error, event, info, warn};
+use tracing::{instrument, trace};
+use wgpu::rwh::{
+    AppKitWindowHandle, HandleError, HasDisplayHandle, HasWindowHandle, RawWindowHandle,
+    WindowHandle,
+};
 use wgpu::wgt::WgpuHasDisplayHandle;
+use wgpu::{
+    Adapter, Device, DeviceDescriptor, Instance, InstanceDescriptor, PowerPreference, Queue,
+    RequestAdapterOptions, Surface,
+};
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, EventLoopProxy};
@@ -14,54 +26,78 @@ use winit::window::{Window, WindowId};
 #[derive(Debug)]
 pub enum UserEvent {
     WakeUp,
+    InitializeState,
 }
 
 #[derive(Debug)]
 pub struct App {
+    oneshot: Option<Sender<Arc<Window>>>,
     window: Option<Arc<Window>>,
-    tx: Option<Sender<Arc<Window>>>,
+    state: Option<State>,
 }
 
 impl App {
-    /// Creates new App struct which implements ApplicationHandler.
-    ///
-    /// This does not initialize the window until event_loop.run_app() is called
-    /// on the App struct, which consequently calls the resumed() ApplicationHandler implementation.
+    #[instrument]
     pub fn new(tx: Sender<Arc<Window>>) -> App {
         App {
+            oneshot: Some(tx),
             window: None,
-            tx: Some(tx),
+            state: None,
         }
     }
 
     fn initialize_window(&mut self, event_loop: &ActiveEventLoop) -> &mut Self {
         self.window = match event_loop.create_window(Window::default_attributes()) {
-            Ok(t) => Some(Arc::new(t)),
-            Err(e) => {
-                error!("Error initializing window: {e}");
+            Ok(window) => Some(Arc::new(window)),
+            Err(err) => {
+                error!(?err, "Error initializing window");
                 None
             }
         };
+        trace!(?self.window, "Successfully initialized window");
         self
     }
 
     /// This function is responsible for sending the window to a worker thread down the
     /// oneshot channel, indicating that the application has been initialized.
-    fn initialize_worker(&mut self) -> &mut Self {
-        let _ = self
-            .tx
-            .take()
-            .unwrap()
-            .send(self.window.as_ref().unwrap().clone());
+    ///
+    /// If the channel has already been consumed, this function does nothing.
+    #[instrument]
+    fn send_window_down_oneshot(&mut self) -> &mut Self {
+        let Some(oneshot) = self.oneshot.take() else {
+            warn!("Oneshot channel has already been consumed!");
+            return self;
+        };
+
+        if let Err(_) = oneshot.send(self.window.as_ref().unwrap().clone()) {
+            error!("Failed to send window over oneshot channel.");
+        }
+
+        trace!("Successfully transmitted window over oneshot channel");
         self
     }
+
+    // pub fn window(&self) -> Arc<Window> {
+    //     self.window
+    //         .as_ref()
+    //         .ok_or_else(|| {
+    //             anyhow!("attempted to access window before the application is initialized.")
+    //         })
+    //         .expect("this function should only be called after the application is run")
+    //         .clone()
+    // }
 }
 
 impl ApplicationHandler<UserEvent> for App {
+    #[instrument]
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
         // initialize window and worker
         if self.window.is_none() {
-            self.initialize_window(event_loop).initialize_worker();
+            trace!("Obtained first resume event");
+            self.initialize_window(event_loop)
+                .send_window_down_oneshot();
+        } else {
+            trace!("Obtained resume event")
         }
     }
 
@@ -74,26 +110,111 @@ impl ApplicationHandler<UserEvent> for App {
         match event {
             // closing the window
             WindowEvent::CloseRequested => event_loop.exit(),
-            //
+
             _ => (),
         }
-
-        // info!("{event_loop:?}");
-        // info!("{window_id:?}");
-        // info!("{event:?}");
     }
 
+    #[instrument]
     fn user_event(&mut self, event_loop: &ActiveEventLoop, event: UserEvent) {
-        info!("{event_loop:?}");
-        info!("{event:?}");
+        let window = self.window.as_ref().unwrap().clone();
+        match event {
+            UserEvent::InitializeState => {
+                self.state = State::new(window, event_loop)
+                    .inspect(|state| {
+                        debug!(?state);
+                    })
+                    .inspect_err(|err| {
+                        error!(%err, "failed to initialize state from user event");
+                    })
+                    .ok();
+            }
+            _ => (),
+        };
     }
 }
 
-impl HasWindowHandle for App {
-    fn window_handle(&self) -> Result<WindowHandle<'_>, HandleError> {
-        match self.window {
-            Some(_) => self.window.as_ref().unwrap().window_handle(),
-            None => Err(HandleError::Unavailable),
-        }
+#[derive(Debug)]
+pub struct State {
+    pub surface: Surface<'static>,
+    pub device: Device,
+    pub queue: Queue,
+}
+
+impl State {
+    fn request_adapter(
+        instance: &Instance,
+        adapter_options: &RequestAdapterOptions,
+    ) -> anyhow::Result<Adapter> {
+        let adapter = instance
+            .request_adapter(&adapter_options)
+            .block_on()
+            .inspect_err(|err| {
+                error!(%err);
+            })
+            .context("requesting adapter")?;
+        debug!(?adapter);
+
+        Ok(adapter)
+    }
+
+    fn request_device_and_queue(
+        adapter: &Adapter,
+        device_descriptor: &DeviceDescriptor,
+    ) -> anyhow::Result<(Device, Queue)> {
+        let (device, queue) = adapter
+            .request_device(&device_descriptor)
+            .block_on()
+            .inspect_err(|err| {
+                error!(%err);
+            })
+            .context("requesting device")?;
+        debug!(?device);
+
+        Ok((device, queue))
+    }
+
+    fn new(window: Arc<Window>, event_loop: &ActiveEventLoop) -> anyhow::Result<State> {
+        // create instance
+        let instance_descriptor = InstanceDescriptor::new_with_display_handle(Box::new(
+            event_loop.owned_display_handle(),
+        ));
+        let instance = Instance::new(instance_descriptor);
+        event!(Level::DEBUG, ?instance);
+
+        let adapter_options = RequestAdapterOptions {
+            power_preference: PowerPreference::None,
+            force_fallback_adapter: false,
+            compatible_surface: None,
+            apply_limit_buckets: false,
+        };
+        let device_descriptor = DeviceDescriptor {
+            label: Some("Metal"),
+            required_features: Default::default(),
+            required_limits: Default::default(),
+            experimental_features: Default::default(),
+            memory_hints: Default::default(),
+            trace: Default::default(),
+        };
+
+        let adapter = Self::request_adapter(&instance, &adapter_options)
+            .inspect_err(|err| error!(%err))
+            .context("requesting adapter")?;
+        let (device, queue) = Self::request_device_and_queue(&adapter, &device_descriptor)
+            .inspect_err(|err| error!(%err))
+            .context("requesting device")?;
+        let surface = instance
+            .create_surface(window)
+            .inspect_err(|err| {
+                error!(%err);
+            })
+            .context("creating surface")?;
+        debug!(?surface);
+
+        Ok(State {
+            surface,
+            device,
+            queue,
+        })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,58 @@
+#![allow(warnings)]
 pub mod app;
-pub mod draw;
 
-use app::{App, UserEvent};
+use app::{App, State, UserEvent};
 
-use log::debug;
+use anyhow::Context;
+use pollster::FutureExt;
 use std::sync::Arc;
+use tokio::runtime::{Handle, Runtime};
 use tokio::sync::oneshot;
+use tokio::task;
+use tokio::task::LocalSet;
+use tracing::{Event, Level, event, info, instrument, span, trace};
+use tracing::{debug, error};
+use tracing_subscriber::fmt::format::FmtSpan;
+use tracing_subscriber::{EnvFilter, FmtSubscriber, filter, fmt};
 use winit::error::EventLoopError;
+use winit::event_loop::{EventLoopProxy, OwnedDisplayHandle};
 use winit::{event_loop::EventLoop, window::Window};
 
+#[instrument]
 #[tokio::main]
-async fn main() -> anyhow::Result<(), EventLoopError> {
-    // initialize logger
-    env_logger::init();
+async fn main() -> anyhow::Result<()> {
+    let _global_subscriber = FmtSubscriber::builder()
+        .pretty()
+        .with_target(true)
+        .with_env_filter(EnvFilter::new("info,graphics=trace"))
+        .with_span_events(FmtSpan::ACTIVE)
+        .with_thread_names(true)
+        .init();
+
+    let (tx, rx) = oneshot::channel();
 
     // instantiate ApplicationHandler implementor
-    let (tx, rx) = oneshot::channel();
     let mut app = App::new(tx);
+    debug!(?app);
 
     let event_loop = EventLoop::<UserEvent>::with_user_event().build()?;
+    trace!("Built event loop.");
+
     let proxy = event_loop.create_proxy();
+    trace!("Created an event loop proxy.");
 
-    debug!("Spawning task");
-    tokio::task::spawn_blocking(|| async move {
-        debug!("Emitting UserEvent");
-
-        // wait to receive the Window from App::resumed()
-        let window = rx.blocking_recv().unwrap();
+    // spawn a blocking task to wait for window initialization
+    let _join_handle = task::spawn_blocking(move || -> anyhow::Result<()> {
+        let _window = rx.blocking_recv()?;
 
         // now we can safely use the proxy.
-        debug!("Obtained window {window:?}");
-        let _ = proxy.send_event(UserEvent::WakeUp);
+        if let Err(err) = proxy.send_event(UserEvent::InitializeState) {
+            error!(?err, "Event loop is closed");
+        }
+        info!("Sent user event");
 
-        worker_main(window);
+        Ok(())
     });
 
-    event_loop.run_app(&mut app)
-}
-
-/// Serve as an entry point for the worker thread
-fn worker_main(window: Arc<Window>) {
-    debug!("This is the worker thread's entry point to the program.");
-    unimplemented!();
-
-    let canvas = draw::Canvas::new(window).await;
+    event_loop.run_app(&mut app).map_err(|err| err.into())
 }
